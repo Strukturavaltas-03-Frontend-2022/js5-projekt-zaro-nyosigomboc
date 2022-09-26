43e0aaacaeadc6882bc7ae70eaad247f5254cc08
Gyozo Miklos <nyosdroid@gmail.com>
2022-09-26 10:04:29 +0200

    validator ok

--- frontend/js/validator.js
+++ frontend/js/validator.js
@@ -4,10 +4,48 @@ const regularExpressions = {
   address: /^\d+( [A-Z][a-z]+){2,}$/,
 };
 
-const validate = (s = '', kind = '') => {
+const validClass = 'validated-passed';
+const invalidClass = 'validated-failed';
+
+export const validate = (s = '', kind = '') => {
   // LOL, ESLint went mad
   if (!Object.prototype.hasOwnProperty.call(regularExpressions, kind)) {
     throw ReferenceError(`Bad validator value: ${kind}`);
   }
   return regularExpressions[kind].test(s);
+};
+
+export const validateAddClass = (element = document, kind = '') => {
+  try {
+    const res = validate(element.value, kind);
+    if (res) {
+      element.classList.add(validClass);
+      element.classList.remove(invalidClass);
+    } else {
+      element.classList.remove(validClass);
+      element.classList.add(invalidClass);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const classRegexp = /^validate-(?<kind>[a-z]+)$/;
+export const addValidators = (parent = document) => {
+  const elements = parent.querySelectorAll('*[class*="validate-"]');
+  elements.forEach((element) => {
+    element.classList.forEach((cl) => {
+      const match = classRegexp.exec(cl);
+      if (match) {
+        const { kind } = match;
+        if (!Object.prototype.hasOwnProperty.call(regularExpressions, kind)) {
+          console.error(`Bad validator value: ${kind}`);
+        } else {
+          element.addEventListener('change', () => {
+            validateAddClass(element, kind);
+          });
+        }
+      }
+    });
+  });
 };
